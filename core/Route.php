d67f80cd35cc65932a2646f70d6d4c65eb5e08b2
Kurason <vanitocurason@gmail.com>
2021-12-06 22:39:47 +0300

    fix route again))

--- core/Route.php
+++ core/Route.php
@@ -10,23 +10,15 @@
 		
 		public function __construct($methodSpacePath, $controllerAtAction)
 		{
-      parseMethodPath($methodSpacePath);
-      parseControllerAction($controllerAction);
+      preg_match("#^(.+) (.+?)$#", $methodSpacePath, $matches);
+      $this->method = $matches[1];
+			$this->path = $matches[2];
+      
+      preg_match("#^(.+)@(.+?)$#", $controllerAtAction, $matches);
+      $this->method = $matches[1];
+			$this->path = $matches[2];
 		}
-		
-    private function parseMethodPath($methodSpacePath)
-    {
-      preg_match("#^(<$method>.*) (<$path>.*)?#", $methodSpacePath, $matches);
-      $this->method = $matches['method'];
-			$this->path = $matches['path'];
-    }
-    
-    private function parseControllerAction($controllerAtAction)
-    {
-      preg_match("#^(<$controller>.*)@(<$action>.*)?#", $controllerAtAction, $matches);
-      $this->method = $matches['controller'];
-			$this->path = $matches['action'];
-    }
+
 		public function __get($property)
 		{
 			return $this->$property;
