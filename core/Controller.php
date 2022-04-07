6c39a04325b99d50eb02e759acfbd489846430a0
vanito-kurason <vanitocurason@gmail.com>
2022-04-07 17:47:31 +0300

    View modification

--- core/Controller.php
+++ core/Controller.php
@@ -6,7 +6,7 @@ class Controller
   protected $layout = 'default';
   protected $title = '';
 
-  protected function render($view, $content)
+  protected function render($view, $content): Page
   {
     return new Page($this->layout, $this->title, $view, $content);
   }
