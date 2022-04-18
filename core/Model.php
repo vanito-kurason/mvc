a661f7f240c3ace2418e44508a02a849f82a308c
vanito-kurason <vanitocurason@gmail.com>
2022-04-19 00:29:10 +0300

    Update Model.php, using PDO

--- core/Model.php
+++ core/Model.php
@@ -1,29 +1,40 @@
 <?php
-	namespace Core;
-	
-	class Model
+namespace Core;
+use PDO;
+
+class Model
+{
+	public static $link;
+
+	public function __construct()
 	{
-		private static $link;
-		
-		public function __construct()
-		{
-			if (!self::$link) {
-				self::$link = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-				mysqli_query(self::$link, "SET NAMES 'utf8'");
+		if (self::$link === NULL) {
+			try {
+				$dns = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME;
+				self::$link = new PDO($dns, DB_USER, DB_PASS);
+				self::$link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				//self::$link->query("SET NAMES" . DB_CHARSET);
+			}
+			catch (\PDOException $e) {
+				print "Error: " . $e->getMessage(). "<br/>";
+				die();
 			}
 		}
-		
-		protected function findOne($query)
-		{
-			$result = mysqli_query(self::$link, $query) or die(mysqli_error(self::$link));
-			return mysqli_fetch_assoc($result);
-		}
-		
-		protected function findMany($query)
-		{
-			$result = mysqli_query(self::$link, $query) or die(mysqli_error(self::$link));
-			for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);
-			
-			return $data;
-		}
 	}
+
+	protected function findOne(string $query, string $name): array
+	{
+		$stmt = self::$link->prepare($query);
+		$stmt->execute([$name]);
+		$data = $stmt->fetch();
+		return $data;
+	}
+
+	protected function findMany(string $query): array
+	{
+		$stmt = self::$link->prepare($query);
+		$stmt->execute();
+		$data = $stmt->fetchAll();
+		return $data;
+	}
+}
